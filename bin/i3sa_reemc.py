3fa1910ca59306636b31f365b0b91f5c30235b08
Felix Aller <felix.aller@ziti.uni-heidelberg.de>
2021-03-19 19:58:15 +0100

    scalar .yaml output of pal pi

--- bin/i3sa_reemc.py
+++ bin/i3sa_reemc.py
@@ -5,12 +5,12 @@ from locomotionbench import utility, metrics
 
 ARG1 = 'conf/experiment.yaml'
 ARG2 = 'conf/robot.yaml'
-
+OUTPUT = 'output/'
 if __name__ == '__main__':
 
     e_yaml = open(ARG1)
     r_yaml = open(ARG2)
-
+    export = utility.PIExporter()
     experiment = yaml.load(e_yaml, Loader=yaml.FullLoader)
     robot = yaml.load(r_yaml, Loader=yaml.FullLoader)
 
@@ -18,4 +18,10 @@ if __name__ == '__main__':
     metrics = exp.calc_metrics()
     distance_traveled, n_steps, normalized_dist_steps = exp.get_n_steps_normalized_by_leg_distance()
     impact = exp.get_impact()
+
+    export.export_scalar(impact, OUTPUT + 'impact.yaml')
+    export.export_scalar(distance_traveled, OUTPUT + 'distance_traveled.yaml')
+    export.export_scalar(n_steps, OUTPUT + 'n_steps.yaml')
+    export.export_scalar(normalized_dist_steps, OUTPUT + 'normalized_dist_steps.yaml')
+
     print('---')
